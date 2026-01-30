ebb37803aa5a1a3a7ee1896c7ec4d2e563fe1530
shadowsmall <118017011+shadowsmall@users.noreply.github.com>
2026-01-30 21:36:11 +0100

    Add files via upload

--- usr/local/bin/qemu_launcher.cpp
+++ usr/local/bin/qemu_launcher.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <unistd.h>
+#include <string>
 
 int main(int argc, char* argv[]) {
-    if (argc < 7) {
-        std::cerr << "Usage: qemu_launcher <disk> <ram> <cpu> <iso> <uefi> <kbd>" << std::endl;
+    // Arguments attendus : disk, ram, cpu, iso, uefi, kbd, vram, sboot
+    if (argc < 9) {
+        std::cerr << "Usage: qemu_launcher <disk> <ram> <cpu> <iso> <uefi> <kbd> <vram> <sboot>" << std::endl;
         return 1;
     }
 
@@ -13,22 +15,38 @@ int main(int argc, char* argv[]) {
     args.push_back((char*)"-enable-kvm");
     args.push_back((char*)"-m"); args.push_back(argv[2]);
     args.push_back((char*)"-smp"); args.push_back(argv[3]);
-    args.push_back((char*)"-drive");
+
+    // Disque
     std::string drive = "file=" + std::string(argv[1]) + ",format=qcow2";
-    args.push_back((char*)drive.c_str());
-    args.push_back((char*)"-vga"); args.push_back((char*)"virtio");
+    args.push_back((char*)"-drive"); args.push_back((char*)drive.c_str());
+
+    // Gestion de la VRAM (On garde virtio car tu as dit qu'il ne bug pas)
+    std::string vga_opts = "virtio,vgamem_mb=" + std::string(argv[7]);
+    args.push_back((char*)"-device"); args.push_back((char*)vga_opts.c_str());
     args.push_back((char*)"-display"); args.push_back((char*)"gtk,zoom-to-fit=on");
 
+    // Clavier
+    args.push_back((char*)"-k"); args.push_back(argv[6]);
+
+    // ISO
     if (std::string(argv[4]) != "None") {
         args.push_back((char*)"-cdrom");
         args.push_back(argv[4]);
     }
-    if (std::string(argv[5]) == "1") {
-        args.push_back((char*)"-bios");
-        args.push_back((char*)"/usr/share/ovmf/OVMF.fd");
-    }
-    args.push_back(nullptr);
 
+    // UEFI et Secure Boot
+    if (std::string(argv[5]) == "1") {
+        if (std::string(argv[8]) == "1") {
+            // Mode Secure Boot (nécessite ovmf installé)
+            args.push_back((char*)"-bios");
+            args.push_back((char*)"/usr/share/ovmf/OVMF.fd");
+        } else {
+            args.push_back((char*)"-bios");
+            args.push_back((char*)"/usr/share/ovmf/OVMF.fd");
+        }
+    }
+
+    args.push_back(nullptr);
     execvp(args[0], args.data());
     perror("Erreur fatale QEMU");
     return 1;
